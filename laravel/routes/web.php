8bc384532c850c822940e8fe243bad09c37a0a73
Vova-test <bezvitryy@gmail.com>
2021-03-14 21:10:07 +0200

    install bootstrap, add new routes, views, css, js for layout.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*Route::get('/', function () {
+    return view('home');
+});*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
+
+// User Authentication Routes
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/test', 'TestController@test')->name('test');
