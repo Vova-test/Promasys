5a970a273cf179774c50a432ee2d4a8525a1e08a
Vova-test <bezvitryy@gmail.com>
2021-03-16 18:59:14 +0200

    changed layouts/app.blade.php, created login page and controller and added routes for them, edit session table.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,18 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('home');
-});*/
-
-Route::get('/', function () {
-    return view('layout');
-});
-
 // User Authentication Routes
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login1');
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/test', 'TestController@test')->name('test');
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::get('/', function () {
+        return view('home');
+    });
+
+    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+});
