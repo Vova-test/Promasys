506319901e92b17017ea5d48c91b026df465e85c
Vova-test <bezvitryy@gmail.com>
2021-06-20 07:38:00 +0300

    edited routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,28 +14,57 @@ use Illuminate\Support\Facades\Route;
 */
 
 // User Authentication Routes
-Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('/login', 'Auth\LoginController@showLoginForm')
+    ->name('login');
 Route::post('/login', 'Auth\LoginController@login');
 
 Route::group(['middleware' => ['auth']], function () {
 
     Route::redirect('/', '/projects');
-    Route::get('/test','ProjectController@test');
+
+    //Route::get('/test','ProjectController@test');
 
     Route::prefix('/projects')->group(function () {
-        Route::get('/','ProjectController@index')->name('projects');
+        Route::get('/','ProjectController@index')
+            ->name('projects');
         Route::post('/','ProjectController@getProjects');
 
-        Route::get('/card/{id}','ProjectController@show')->name('project');
-        Route::get('/{id}/card','ProjectController@show')->name('card');
+        Route::post('/store','ProjectController@store')
+            ->name('project.store');
+        Route::post('/update/{id}','ProjectController@update')
+            ->name('project.update');
+        Route::delete('/delete/{id}','ProjectController@destroy')
+            ->name('project.delete');
 
-        Route::post('/store','ProjectController@store')->name('project.store');
-        Route::post('/update/{id}','ProjectController@update')->name('project.update');
-
-        Route::delete('/delete/{id}','ProjectController@destroy')->name('project.delete');
+        Route::get('/card/{id}','ProjectController@show')
+            ->name('project.card');
     });
 
-    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    Route::prefix('/credentials')->group(function () {
+        Route::post('/list/{project}','CredentialSetController@getCredentials')
+            ->name('credentials');
 
-    Route::post('panic', 'PanicController@index')->name('panic');
+        Route::post('/store','CredentialSetController@store')
+            ->name('credential.store');
+
+        Route::delete('/delete/{id}','CredentialSetController@destroy')
+            ->name('credential.delete');
+    });
+
+    Route::prefix('/settings')->group(function () {
+        Route::get('/{project}','ProjectSettingController@index')
+            ->name('settings');
+        Route::post('/{project}','ProjectSettingController@getSettings');
+
+        Route::post('/store','ProjectSettingController@store')
+            ->name('settings.store');
+        Route::delete('/delete/{id}','ProjectSettingController@destroy')
+            ->name('settings.delete');
+    });
+
+    Route::post('logout', 'Auth\LoginController@logout')
+        ->name('logout');
+
+    Route::post('panic', 'PanicController@index')
+        ->name('panic');
 });
